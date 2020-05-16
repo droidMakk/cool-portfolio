bd0705ac38702acd1a5632dec7dc74fe9ea285f5
droidmakk <droidmakk@gmail.com>
2020-05-17 04:12:59 +0530

    service worker and favicons with manifest.json

---
+++ src/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('droidmakk').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/assets/css/slicktheme.css',
+                '/main.css',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
